a82a613cf784f562324cdc7a7142d8662ab4ce0d
WeiChun <weichun@csie.tw>
2017-12-07 16:06:50 +0800

    Init

---
+++ PDFReplacer/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace PDFReplacer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            //
+            // label1
+            //
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            //
+            // textBox1
+            //
+            this.textBox1.Location = new System.Drawing.Point(51, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(227, 22);
+            this.textBox1.TabIndex = 1;
+            //
+            // openFileDialog1
+            //
+            this.openFileDialog1.DefaultExt = "*.pdf";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            //
+            // button1
+            //
+            this.button1.Location = new System.Drawing.Point(285, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            //
+            // label2
+            //
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pages: ";
+            //
+            // label3
+            //
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            //
+            // button2
+            //
+            this.button2.Location = new System.Drawing.Point(285, 318);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            //
+            // dataGridView1
+            //
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[]
+            {
+                this.Column1,
+                this.Column2
+            });
+            this.dataGridView1.Location = new System.Drawing.Point(12, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(348, 242);
+            this.dataGridView1.TabIndex = 6;
+            //
+            // Column1
+            //
+            this.Column1.HeaderText = "Page No";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            //
+            // Column2
+            //
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "ImagePath";
+            this.Column2.Name = "Column2";
+            //
+            // button3
+            //
+            this.button3.Location = new System.Drawing.Point(285, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Open Images";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            //
+            // openFileDialog2
+            //
+            this.openFileDialog2.Filter = "Image|*.jpg; *.png";
+            this.openFileDialog2.Multiselect = true;
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            //
+            // saveFileDialog1
+            //
+            this.saveFileDialog1.Filter = "PDF File | *.pdf";
+            //
+            // Form1
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 364);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ PDFReplacer/Form1.cs
@@ -0,0 +1,108 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.xtra.iTextSharp.text.pdf.pdfcleanup;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDFReplacer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            var path = openFileDialog1.FileName;
+            textBox1.Text = path;
+            PdfReader reader = new PdfReader(path);
+            label3.Text = reader.NumberOfPages.ToString();
+            var rows = dataGridView1.Rows;
+            rows.Clear();
+
+            for (int i = 1, j = reader.NumberOfPages; i <= j; i++)
+            {
+                rows.Add(i, "");
+            }
+
+            reader.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK)
+                return;
+
+            var reader = new PdfReader(textBox1.Text);
+            var dstPath = saveFileDialog1.FileName;
+            var rows = dataGridView1.Rows;
+            var bookmarks = SimpleBookmark.GetBookmark(reader);
+            var dst = new PdfStamper(reader, new FileStream(dstPath, FileMode.Create))
+            {
+                XmpMetadata = reader.Metadata,
+                Outlines = bookmarks
+            };
+
+            for (int i = 0, j = rows.Count; i < j; i++)
+            {
+                var cell = rows[i].Cells;
+                var pageNo = (int)cell[0].Value;
+                var imgPath = (string)cell[1].Value;
+
+                if (imgPath != null && imgPath != "")
+                {
+                    var rect = reader.GetPageSize(pageNo);
+                    Image image = Image.GetInstance(imgPath);
+                    image.SetAbsolutePosition(0, 0);
+                    image.ScaleAbsolute(rect);
+                    var cleanUpLocations = new List<PdfCleanUpLocation> { new PdfCleanUpLocation(pageNo, rect, BaseColor.WHITE) };
+                    new PdfCleanUpProcessor(cleanUpLocations, dst).CleanUp();
+                    var template = PdfTemplate.CreateTemplate(dst.Writer, image.Width, image.Height);
+                    template.AddImage(image);
+                    dst.GetOverContent(pageNo).AddTemplate(template, 0, 0, true);
+                }
+            }
+
+            dst.Close();
+            reader.Close();
+            MessageBox.Show("Done");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.ShowDialog();
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+            var rows = dataGridView1.Rows;
+            string[] tmp = openFileDialog2.FileNames;
+            Array.Sort<string>(tmp);
+            int i = 0;
+
+            foreach (var str in openFileDialog2.FileNames)
+            {
+                if (i < rows.Count)
+                {
+                    rows[i].Cells[1].Value = str;
+                }
+
+                i++;
+            }
+        }
+    }
+}
